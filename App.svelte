399e27be5b15e27eaacfa2253ca9c41e6207040f
himanushi <himanushi@users.noreply.github.com>
2021-02-26 06:25:16 +0900

    一旦動く

---
+++ App.svelte
@@ -0,0 +1,20 @@
+<!-- App.svelte -->
+<script>
+  /* component logic will go here */
+</script>
+<style>
+  /* css will go here */
+</style>
+<div class="App">
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Svelte
+    </a>
+  </header>
+  成功
+</div>
---
+++ index.js
@@ -0,0 +1,8 @@
+/* Add JavaScript code here! */
+import App from "./App.svelte";
+
+let app = new App({
+  target: document.body,
+});
+
+export default app;
