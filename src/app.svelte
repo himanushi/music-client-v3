da465e63f7da06d8d2080378ac6939ca0d7ccb6c
himanushi <himanushi@users.noreply.github.com>
2021-03-07 06:48:02 +0900

    typescript 対応

---
+++ src/app.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const a:number = 1
+</script>
+
+<div class="App">
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Svelte
+    </a>
+  </header>
+  成功
+</div>
