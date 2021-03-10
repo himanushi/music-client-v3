ec3a24b14564c78ac219f170f4e84454e6ae93a0
himanushi <himanushi@users.noreply.github.com>
2021-03-11 06:40:10 +0900

    eslint と tailwind の相性が悪いところを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   "purge": {
     "content": ["./src/**/*.svelte"]
   },
+
+  /*
+   * eslint では、CSS で ":" を使用するとパーサーエラーが発生することがあります。
+   * In eslint, using ":" in CSS may cause a parser error.
+   * @apply focus:outline-non → @apply focus_outline-non
+   */
+  "separator": "_",
   "theme": {},
   "variants": {}
 };
