5a71fca8b8c8789c38976b7355cbc3cca3289b72
himanushi <himanushi@users.noreply.github.com>
2021-03-11 16:28:37 +0900

    main 変更

--- src/app.svelte
+++ src/app.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "~/styles/base.svelte";
-  import Card from "~/components/card.svelte";
+
+  import Main from "~/pages/main.svelte";
 </script>
 
-<Card />
+<Main />
---
+++ src/pages/main.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Content from "~/components/content.svelte";
+  import Footer from "~/components/footer.svelte";
+  import Header from "~/components/header.svelte";
+</script>
+
+<Content>
+  <Header />
+  <Content />
+  <Footer />
+</Content>
