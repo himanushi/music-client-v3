b2a3e69b7393fef58151c5057ddfe55a79cf1b53
himanushi <himanushi@users.noreply.github.com>
2021-03-26 11:36:18 +0900

    image 修正

--- src/components/square-image.svelte
+++ src/components/square-image.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
-  export let src: string =
-    // eslint-disable-next-line max-len
-    "https://is5-ssl.mzstatic.com/image/thumb/Music124/v4/5c/11/ff/5c11ff83-95a3-3db7-698a-5e1d692415b8/SQEX-10850_3000x3000.jpg/640x640bb.jpeg";
+  import Photograph from "~/icons/photograph.svelte";
+
+  export let src: string | null | undefined = null;
   export let alt = "no description";
   export let size = 10;
+
+  let style = "";
+
+  if (src) {
+
+    style = `background-image: url("${src}");`;
+
+  }
 </script>
 
-<img {src} {alt} class="h-{size} w-{size} inline rounded" />
+<div {style} {alt} class="h-{size} w-{size}">
+  {#if !src}
+    <Photograph size={6} />
+  {/if}
+</div>
+
+<style lang="scss">
+  div {
+    @apply block rounded bg-gray-600;
+
+    /* img */
+    @apply bg-cover bg-center bg-no-repeat;
+  }
+</style>
