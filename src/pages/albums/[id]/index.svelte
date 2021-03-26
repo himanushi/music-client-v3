74ea036f1ea0c92892f5c2620dfb2170d7301ddf
himanushi <himanushi@users.noreply.github.com>
2021-03-26 11:36:35 +0900

    アルバム詳細画面追加

--- src/pages/albums/[id]/index.svelte
+++ src/pages/albums/[id]/index.svelte
@@ -1,1 +1,27 @@
-<div>album info</div>
+<script lang="ts">
+  import { query } from "svelte-apollo";
+  import Image from "~/components/square-image.svelte";
+  import { AlbumDocument } from "~/graphql/types";
+  import type { Album, AlbumQuery } from "~/graphql/types";
+
+  export let id = "";
+
+  const albumQuery = query<AlbumQuery>(AlbumDocument, {
+    "fetchPolicy": "cache-first",
+    "variables": { id }
+  });
+
+  let album: Album | undefined;
+
+  $: if ($albumQuery.data) {
+
+    album = $albumQuery.data.album as Album;
+
+  }
+</script>
+
+{#if album && album.artworkL.url}
+  <article>
+    <Image src={album.artworkL.url} alt={album.name} size={18} />
+  </article>
+{/if}
