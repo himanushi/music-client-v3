02535970174185f3af063e6543fd72ad00bde6b9
himanushi <himanushi@users.noreply.github.com>
2021-03-26 11:34:46 +0900

    image icon 追加

---
+++ src/icons/photograph.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { size as defaultSize, color as defaultColor } from "./default";
+  export let size = defaultSize;
+  export let color = defaultColor;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  class="text-{color} h-{size} w-{size}"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+  />
+</svg>
