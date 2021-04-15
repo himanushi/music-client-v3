2fd562f126ccad57a9057a550c3bb800de93a629
himanushi <himanushi@users.noreply.github.com>
2021-04-15 15:55:44 +0900

    footer でプレイヤー制御

--- src/pages/_footer.svelte
+++ src/pages/_footer.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 import IconButton from "~/components/icon-button.svelte";
 import Image from "~/components/square-image.svelte";
+import PuaseIcon from "~/icons/pause.svelte";
 import PlayIcon from "~/icons/play.svelte";
 import SkipIcon from "~/icons/skip.svelte";
+import { playerService } from "~/machines/jukebox-machine";
+
+const play_or_pause = () => {
+
+  playerService.send("PLAY_OR_PAUSE");
+
+};
+
+const skip = () => {
+
+  playerService.send("NEXT_PLAY");
+
+};
 </script>
 
 <footer>
@@ -15,11 +29,15 @@ import SkipIcon from "~/icons/skip.svelte";
     </div>
   </div>
 
-  <IconButton>
-    <PlayIcon color="text-gray-900" />
+  <IconButton on:click={play_or_pause}>
+    {#if $playerService.value === "playing"}
+      <PuaseIcon color="text-gray-900" />
+    {:else}
+      <PlayIcon color="text-gray-900" />
+    {/if}
   </IconButton>
 
-  <IconButton>
+  <IconButton on:click={skip}>
     <SkipIcon color="text-gray-900" />
   </IconButton>
 </footer>
