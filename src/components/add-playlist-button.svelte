7f44839fa2cc708076d83626ebe23e8a8baf9d78
himanushi <himanushi@users.noreply.github.com>
2021-05-11 12:42:10 +0900

    playkist 追加ボタン側追加

---
+++ src/components/add-playlist-button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import IconButton from "~/components/icon-button.svelte";
+import type { Track } from "~/graphql/types";
+import AddPlaylistIcon from "~/icons/add-playlist.svelte";
+
+export let track: Track;
+</script>
+
+<IconButton>
+  <AddPlaylistIcon />
+</IconButton>
