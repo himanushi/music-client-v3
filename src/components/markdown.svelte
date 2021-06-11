c9f2234fa64eed378aa0598359de31f522c17fce
himanushi <himanushi@users.noreply.github.com>
2021-06-11 14:41:07 +0900

    真ん中に markdown が来るようにした

--- src/components/markdown.svelte
+++ src/components/markdown.svelte
@@ -3,6 +3,14 @@ let className = "";
 export { className as class };
 </script>
 
-<div class={`prose ${className}`}>
-  <slot />
+<div class="markdown">
+  <div class={`prose ${className}`}>
+    <slot />
+  </div>
 </div>
+
+<style lang="scss">
+.markdown {
+  @apply flex justify-center;
+}
+</style>
