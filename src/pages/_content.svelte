2f3d1ca937a78d39c55fcf92b2c41369b21ee234
himanushi <himanushi@users.noreply.github.com>
2021-06-23 02:33:48 +0900

    ios だと表示がバグるので修正

--- src/pages/_content.svelte
+++ src/pages/_content.svelte
@@ -5,16 +5,10 @@ import ChangePage from "./_change-page.svelte";
 let content: HTMLElement;
 
 setContext("content", { getElement: () => content });
-
-// 一瞬だけスクロールの一番上が表示されるのを回避する
-let invisible = true;
 </script>
 
-<main data-routify="scroll-lock" bind:this={content} class:invisible>
-  <ChangePage
-    on:pageChange={() => invisible = true}
-    on:pageLoad={() => invisible = false}
-  />
+<main data-routify="scroll-lock" bind:this={content}>
+  <ChangePage />
   <div class="content">
     <slot />
   </div>
