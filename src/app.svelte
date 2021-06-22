bea4ee10b3884f1f6fbfdc93f992fcc87f14dd5d
himanushi <himanushi@users.noreply.github.com>
2021-06-23 06:14:39 +0900

    100vh対応

--- src/app.svelte
+++ src/app.svelte
@@ -3,6 +3,21 @@ import "~/app.css";
 
 import { Router } from "@roxi/routify";
 import { routes } from "routify/routes";
+import { onMount } from "svelte";
+
+onMount(() => {
+
+  // ref: https://css-tricks.com/the-trick-to-viewport-units-on-mobile
+  const resize = () => {
+
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  };
+  window.addEventListener("resize", resize);
+  resize();
+
+});
 </script>
 
 <Router {routes} />
